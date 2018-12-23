08d5a328689b0d1f96c772bb77675ca02238fff9
dorothybelle247 <dorothybelle247@gmail.com>
2018-12-22 22:47:48 -0500

    haha

---
+++ assets/javascript/app.js
@@ -0,0 +1,75 @@
+
+         document.body.style.backgroundImage = "url('https://cdn1.iconfinder.com/data/icons/confectionery-color/295/05-512.png')";
+
+        //function buildQuiz(){}
+
+        //function showResults(){}
+
+        // display quiz right away
+        //buildQuiz();
+
+        // on submit, show results
+        //submitButton.addEventListener('click', showResults);
+        // var correctAnswer = 0;
+
+        // var wrongAnswer = 0;
+
+        // var question = 0;
+
+        // var images;
+
+        // var count = 30;
+
+        // var myQuestions = [
+        //     {
+        //         question: "Who ?",
+        //         answers: {
+        //             a: "Nikola Tesla",
+        //             b: "Thomas Edison",
+        //             c: "James Madison"
+        //         },
+        //         correctAnswer: "a"
+        //     },
+        //     {
+        //         question: "What is the best site ever created?",
+        //         answers: {
+        //             a: "SitePoint",
+        //             b: "Simple Steps Code",
+        //             c: "Trick question; they're both the best"
+        //         },
+        //         correctAnswer: "c"
+        //     },
+        //     {
+        //         question: "Where is Waldo really?",
+        //         answers: {
+        //             a: "Antarctica",
+        //             b: "Exploring the Pacific Ocean",
+        //             c: "Sitting in a tree",
+        //             d: "Minding his own business, so stop asking"
+        //         },
+        //         correctAnswer: "d"
+        //     }
+        // ];
+
+       function submitAnswers(){
+           var total=5;
+           var score=0;
+
+           var q1= document.forms["quizform"]["q1"].value;
+           var q2= document.forms["quizform"]["q2"].value;
+           var q3= document.forms["quizform"]["q3"].value;
+           var q4= document.forms["quizform"]["q4"].value;
+           var q5= document.forms["quizform"]["q5"].value;
+        
+
+       r(i=1; i<=total; i++ ){
+        if(q1 ==null || q1=='' ){
+            alert("you missed a question 1");
+            return false;
+        
+        }
+       fo
+
+       }
+        }
+   
